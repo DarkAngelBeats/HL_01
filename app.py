6a3d8060c8455a260a24b522a61c63ab5c9c7a0a
DarkAngelBeats <94038624+DarkAngelBeats@users.noreply.github.com>
2022-01-03 17:15:15 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,163 @@
+from datetime import datetime
+from functools import wraps
+from flask import Flask, render_template, request, abort, make_response, session, url_for, redirect, flash
+import model
+from templates import config
+
+app = Flask(__name__)
+
+app.secret_key = 'BAD_SECRET_KEY'
+
+tech_token_list = dict()
+tech_token_life = 15
+
+'''
+def token_check(token):
+    # Проверка токена (времени жизни 15 минут)
+    if token in tech_token_list:
+        if datetime.now() - tech_token_list[token] < tech_token_life:
+            return True
+        else:
+            del tech_token_list[token]
+    return False
+'''
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        print("route login_required")
+        if session.get('userid'):
+            print(f'Logged in as {session["userid"]}')
+        else:
+            print('You are not logged in')
+            return redirect(url_for('index_page'))
+
+        # return abort(401, description="Ошибка авторизации (не совпадают данные подключения)")
+        # if not token_check(token):
+        #    return abort(400, description="Token error")
+
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+@app.errorhandler(404)
+def not_found_page(error):
+    return render_template('404.html'), 404
+
+
+@app.route("/")
+def index_page():
+    print("route index")
+    if session.get('userid'):
+        return redirect(url_for('profile_page'))
+
+    # Доступ к параметрам
+    # searchword = request.args.get('key', '')
+    resp = make_response(render_template('index.html'))
+    return resp
+
+
+@app.route('/login', methods=['POST'])
+# @login_required
+def login():
+    print("route login")
+    if session.get('userid'):
+        return redirect(url_for('profile_page'))
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        print(username, password)
+        if model.valid_login(username, password):
+            return url_for('profile_page')
+        else:
+            flash('Invalid username/password')
+            return url_for('index_page')
+    else:
+        return render_template('index.html')
+
+
+@app.route("/profile")
+@login_required
+def profile_page():
+    print("route profile")
+    user_res = model.profile_get(session.get('userid'))
+    return render_template('profile.html', recnew=False, data=user_res, cities=None)  # , paraKey=paraValue
+
+
+@app.route("/profile/new")
+def profile_new_page():
+    print("route profile_new_page")
+    if session.get('userid'):
+        return redirect(url_for('profile_page'))
+
+    cities_res = model.cities_list()
+    return render_template('profile.html', recnew=True, data=None, cities=cities_res)
+
+
+@app.route("/profile/set", methods=['POST'])
+def profile_set():
+    print("route profile_set")
+    if session.get('userid'):
+        return redirect(url_for('profile_page'))
+
+    firstname = request.form['firstname']
+    lastname = request.form['lastname']
+    age = request.form['age']
+    sex = request.form['sex']
+    interests = request.form['interests']
+    city = request.form['city']
+    email = request.form['email']
+    password = request.form['password']
+    print(firstname, lastname, age, sex, interests, city, email, password)
+    data = [firstname, lastname, age, sex, city, email, password]
+    if model.profile_set(data):
+        userid = session.get('userid')
+        print("New User", userid)
+        int_ids = model.interests_set(interests)
+        model.user_interests_set(int_ids)
+
+    return url_for('profile_page')
+
+
+@app.route('/logout')
+def logout():
+    print("route logout")
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    session.pop('userid', None)
+    session.pop('token', None)
+    return redirect(url_for('index_page'))
+
+
+@app.route('/users')
+@app.route('/users/<int:page>')
+@login_required
+def users_page(page=1):
+    print("route users")
+    users_res, rowcount = model.users_list(page, config.items_per_page)
+    return render_template('users.html', page=page, users=users_res, rowcount=rowcount, items_per_page=config.items_per_page)
+
+
+@app.route('/friends/set', methods=['POST'])
+@login_required
+def friends_set():
+    print("route friends_set")
+    friendid = request.form['friendid']
+    model.friends_set(friendid)
+    return "friends_set"
+
+
+@app.route('/friends/del', methods=['POST'])
+@login_required
+def friends_del():
+    print("route friends_del")
+    friendid = request.form['friendid']
+    model.friends_del(friendid)
+    return "friends_del"
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host="0.0.0.0") #go to http://127.0.0.1:5000/ to view the page.
---
+++ model.py
@@ -0,0 +1,233 @@
+import hashlib
+import secrets
+import mysql
+from flask import session, request
+from mysql.connector import connect, Error
+
+
+def get_con():
+    conn = connect(
+        host="localhost",
+        user="root",
+        password="qazsewsxdr12345",
+        database="dbo")
+    return conn
+
+
+def valid_login(username, password):
+    result = []
+    try:
+        conn = get_con()
+        cursor = conn.cursor()
+        pass_hash = hashlib.md5(password.encode('utf-8')).hexdigest()
+        args = [username, pass_hash, 0]
+        result = cursor.callproc('auth', args)
+
+        print("Printing callproc results", result[2])
+    except mysql.connector.Error as error:
+        print("Failed to execute stored procedure: {}".format(error))
+    except Error as e:
+        print(e)
+    finally:
+        if conn.is_connected():
+            cursor.close()
+            conn.close()
+            print("MySQL connection is closed")
+
+    if len(result) < 2:
+        return None
+    if result[2] > 0:
+        session['username'] = username
+        session['userid'] = result[2]
+        session['token'] = secrets.token_hex()
+    return result[2] > 0
+
+
+def profile_set(data):
+    try:
+        conn = get_con()
+        cursor = conn.cursor()
+        data[6] = hashlib.md5(data[6].encode('utf-8')).hexdigest()
+        args = [*data, 0]
+        result = cursor.callproc('user_add', args)
+        conn.commit()
+
+        print("Printing callproc results", result[7])
+    except mysql.connector.Error as error:
+        print("Failed to execute stored procedure: {}".format(error))
+        return None
+    except Error as e:
+        print(e)
+        return None
+    finally:
+        if conn.is_connected():
+            cursor.close()
+            conn.close()
+            print("MySQL connection is closed")
+
+    if len(result) < 8:
+        return None
+    session['userid'] = result[7]
+    session['token'] = secrets.token_hex()
+    return result[7] > 0
+
+
+def interests_set(data):
+    try:
+        conn = get_con()
+        cursor = conn.cursor()
+
+        args = data.split(',')
+        args = [(i, 0) for i in args]
+        #args = [('Шахматы', 0)]
+        #records = cursor.callproc('interests_set', args)
+        #query = "call interests_set(%s, %s)"
+        #records = cursor.executemany(query, args)
+        ids = []
+        for value in args:
+            result = cursor.callproc('interests_set', value)
+            ids.append(result[1])
+
+        conn.commit()
+        #for result in cursor.stored_results():
+        #    records = result.fetchall()
+        #print(ids)
+    except mysql.connector.Error as error:
+        print("Failed to execute stored procedure: {}".format(error))
+    except Error as e:
+        print(e)
+    finally:
+        if conn.is_connected():
+            cursor.close()
+            conn.close()
+            print("MySQL connection is closed")
+
+    return ids
+
+
+def user_interests_set(data):
+    try:
+        conn = get_con()
+        cursor = conn.cursor()
+
+        args = [(session.get('userid'), i) for i in data]
+        query = "call user_interests_set(%s, %s)"
+        cursor.executemany(query, args)
+        conn.commit()
+        #for result in cursor.stored_results():
+        #    records = result.fetchall()
+    except mysql.connector.Error as error:
+        print("Failed to execute stored procedure: {}".format(error))
+    except Error as e:
+        print(e)
+    finally:
+        if conn.is_connected():
+            cursor.close()
+            conn.close()
+            print("MySQL connection is closed")
+    return True
+
+
+def friends_set(friendid):
+    try:
+        conn = get_con()
+        cursor = conn.cursor()
+
+        args = [session.get('userid'), friendid]
+        cursor.callproc("friends_set", args)
+        conn.commit()
+    except mysql.connector.Error as error:
+        print("Failed to execute stored procedure: {}".format(error))
+    except Error as e:
+        print(e)
+    finally:
+        if conn.is_connected():
+            cursor.close()
+            conn.close()
+            print("MySQL connection is closed")
+    return True
+
+
+def friends_del(friendid):
+    try:
+        conn = get_con()
+        cursor = conn.cursor()
+
+        args = [session.get('userid'), friendid]
+        cursor.callproc("friends_del", args)
+        conn.commit()
+    except mysql.connector.Error as error:
+        print("Failed to execute stored procedure: {}".format(error))
+    except Error as e:
+        print(e)
+    finally:
+        if conn.is_connected():
+            cursor.close()
+            conn.close()
+            print("MySQL connection is closed")
+    return True
+
+
+def cities_list():
+    try:
+        conn = get_con()
+        cursor = conn.cursor()
+        cursor.callproc('cities_list')
+
+        for result in cursor.stored_results():
+            records = result.fetchall()
+    except mysql.connector.Error as error:
+        print("Failed to execute stored procedure: {}".format(error))
+    except Error as e:
+        print(e)
+    finally:
+        if conn.is_connected():
+            cursor.close()
+            conn.close()
+            print("MySQL connection is closed")
+
+    return records
+
+
+def users_list(page, items_per_page):
+    try:
+        conn = get_con()
+        cursor = conn.cursor()
+        args = [session.get("userid"), page, items_per_page, 0]
+        res = cursor.callproc('users_list', args)
+
+        for result in cursor.stored_results():
+            records = result.fetchall()
+    except mysql.connector.Error as error:
+        print("Failed to execute stored procedure: {}".format(error))
+    except Error as e:
+        print(e)
+    finally:
+        if conn.is_connected():
+            cursor.close()
+            conn.close()
+            print("MySQL connection is closed")
+
+    return records, res[3]
+
+
+def profile_get(userid):
+    try:
+        conn = get_con()
+        cursor = conn.cursor()
+        args = [userid]
+        cursor.callproc('user_get', args)
+
+        for result in cursor.stored_results():
+            records = result.fetchall()
+    except mysql.connector.Error as error:
+        print("Failed to execute stored procedure: {}".format(error))
+    except Error as e:
+        print(e)
+    finally:
+        if conn.is_connected():
+            cursor.close()
+            conn.close()
+            print("MySQL connection is closed")
+
+    return records[0]
